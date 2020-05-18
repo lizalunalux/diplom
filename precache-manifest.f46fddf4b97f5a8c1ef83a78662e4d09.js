e0958af1d47c4d2e69c2b60d22538405a7568e97
Yelyzaveta Khokhlova <yelyzaveta_khokhlova@epam.com>
2020-05-18 12:57:37 +0300

    Updates

---
+++ precache-manifest.f46fddf4b97f5a8c1ef83a78662e4d09.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd402a7cf24e12b479775f7f1511ec48",
+    "url": "/diplom/index.html"
+  },
+  {
+    "revision": "989c0fe3b21eb91a5c08",
+    "url": "/diplom/static/css/main.88b355f9.chunk.css"
+  },
+  {
+    "revision": "334ccaeba9915b2f139a",
+    "url": "/diplom/static/js/2.7a573abd.chunk.js"
+  },
+  {
+    "revision": "498ad69a5652946930a7e14e08d23238",
+    "url": "/diplom/static/js/2.7a573abd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "989c0fe3b21eb91a5c08",
+    "url": "/diplom/static/js/main.ba0dd2b7.chunk.js"
+  },
+  {
+    "revision": "79094e2ec1123954875f",
+    "url": "/diplom/static/js/runtime-main.a4e94be4.js"
+  },
+  {
+    "revision": "af7185c1bd0101208bd21019073ee409",
+    "url": "/diplom/static/media/bg.af7185c1.jpg"
+  },
+  {
+    "revision": "96be13b934a58c9e888b4290cea41e95",
+    "url": "/diplom/static/media/branding.96be13b9.svg"
+  }
+]);
